020d28d0bc97e73da22695b82f01a4da35a1e194
liuda <liudan910@foxmail.com>
2016-10-07 14:02:25 +0800

    只保留java包

---
+++ src/Container/AboutArrayList.java
@@ -0,0 +1,17 @@
+package Container;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by liuda on 2016/10/7.
+ */
+public class AboutArrayList {
+    public static void main(String[] args){
+        ArrayList<Integer> xx = new ArrayList<Integer>(Arrays.asList(2,3,5,7,0));
+        List<Integer> yy = (List<Integer>) xx.clone();
+        int intT[]  =  new int[0];
+    }
+}
